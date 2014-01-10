473759307f4bc19ddf2ee4992d6229dc777940c1
Chris Hopman <cjhopman@google.com>
2014-01-10 13:17:32 -0800

    Create a minimal GWT app
    This is a very simple gwt app.
    
    Run ant devmode to confirm that compilation and the app works (should see a
    blank page at first then the GWT logging widget with the logged "It
    works").
    
    Run ant test to confirm that the JUnit testing framework works (should run a
    single test for both debug and release).
    
    Run ant clean to delete all that output junk.
    
    R=yfriedman@chromium.org
    
    Committed: 9e120ff
    
    Committed: 68bb6e7
    
    Review URL: https://codereview.appspot.com/38560047

---
+++ src/com/dom_distiller/client/DomDistiller.java
@@ -0,0 +1,23 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package com.dom_distiller.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+import java.util.logging.Logger;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class DomDistiller implements EntryPoint {
+  static Logger logger = Logger.getLogger("DomDistiller");
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+      logger.info("It works.");
+  }
+}
---
+++ test/com/dom_distiller/client/DomDistillerTest.java
@@ -0,0 +1,25 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package com.dom_distiller.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * Simple tests.
+ */
+public class DomDistillerTest extends GWTTestCase {
+    @Override
+    public String getModuleName() {
+        return "com.dom_distiller.DomDistillerJUnit";
+    }
+
+    /**
+     * Simple sanity test.
+     */
+    public void testStub() {
+        assertFalse(false);
+        assertTrue(true);
+    }
+}
