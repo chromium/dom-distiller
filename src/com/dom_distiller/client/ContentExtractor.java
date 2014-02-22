5fc15eb7483a6207f024aeaefffe14d73d0f5bb7
Chris Hopman <cjhopman@google.com>
2014-02-21 16:21:01 -0800

    Add html and image extraction to ContentExtractor
    This makes DomToSaxParser calculate an in-order list of text nodes.
    Boilerpipe's TextBlocks contain a list of the indexes of text nodes that
    that block corresponds to. This allows us to map a TextBlock back to the
    corresponding DOM nodes.
    
    Then, this simply uses RelevantImageFinder and NodeListExpander
    to get images and full html.
    
    R=nyquist@chromium.org
    
    Review URL: https://codereview.appspot.com/59220050

--- src/com/dom_distiller/client/ContentExtractor.java
+++ src/com/dom_distiller/client/ContentExtractor.java
@@ -4,12 +4,17 @@
 
 package com.dom_distiller.client;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Node;
 
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import de.l3s.boilerpipe.document.TextBlock;
 import de.l3s.boilerpipe.document.TextDocument;
 import de.l3s.boilerpipe.extractors.CommonExtractors;
 import de.l3s.boilerpipe.sax.BoilerpipeHTMLContentHandler;
@@ -17,8 +22,8 @@ import de.l3s.boilerpipe.sax.BoilerpipeHTMLContentHandler;
 import org.timepedia.exporter.client.Export;
 import org.timepedia.exporter.client.Exportable;
 
-import org.xml.sax.SAXException;
 import org.xml.sax.AttributesImpl;
+import org.xml.sax.SAXException;
 
 @Export()
 public class ContentExtractor implements Exportable {
@@ -26,11 +31,12 @@ public class ContentExtractor implements Exportable {
 
     public static String extractContent() {
         BoilerpipeHTMLContentHandler htmlParser = new BoilerpipeHTMLContentHandler();
+        List<Node> textNodes = null;
 
         try {
             htmlParser.startDocument();
             Element documentElement = Document.get().getDocumentElement();
-            DomToSaxParser.parse(documentElement, htmlParser);
+            textNodes = DomToSaxParser.parse(documentElement, htmlParser);
             htmlParser.endDocument();
         } catch (SAXException e) {
             logger.warning("Parsing failed.");
@@ -44,6 +50,29 @@ public class ContentExtractor implements Exportable {
             logger.warning("Processing failed.");
             return "";
         }
-        return document.getContent();
+
+        List<Integer> contentTextIndexes = new ArrayList<Integer>();
+        for (TextBlock tb : document.getTextBlocks()) {
+            contentTextIndexes.addAll(tb.getContainedTextElements());
+        }
+        Collections.sort(contentTextIndexes);
+
+        // Boilerpipe's text node indexes start at 1.
+        List<Node> contentNodes = new ArrayList<Node>(contentTextIndexes.size());
+        for (Integer i : contentTextIndexes) {
+            contentNodes.add(textNodes.get(i - 1));
+        }
+
+        List<Node> contentAndImages = RelevantImageFinder.findAndAddImages(
+                contentNodes, Document.get().getDocumentElement());
+
+        Node clonedSubtree = NodeListExpander.expand(contentAndImages).cloneSubtree();
+
+        if (clonedSubtree.getNodeType() != Node.ELEMENT_NODE) {
+            return "";
+        }
+        // TODO(cjhopman): this discards the top element and just returns its children. This might
+        // break in some cases.
+        return Element.as(clonedSubtree).getInnerHTML();
     }
 }
--- src/com/dom_distiller/client/DomToSaxParser.java
+++ src/com/dom_distiller/client/DomToSaxParser.java
@@ -15,20 +15,25 @@ import org.xml.sax.AttributesImpl;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
  * Used to generate sax events from the DOM tree.
  */
-class DomToSaxParser {
+public class DomToSaxParser {
     static Logger logger = Logger.getLogger("DomToSaxParser");
+
 
     private static class DomToSaxVisitor implements DomWalker.Visitor {
         private static final String sHtmlNamespace = "http://www.w3.org/1999/xhtml";
         private final ContentHandler handler;
+        private List<Node> textNodes;
 
         DomToSaxVisitor(ContentHandler h) {
             handler = h;
+            textNodes = new ArrayList<Node>();
         }
 
         @Override
@@ -36,6 +41,7 @@ class DomToSaxParser {
             try {
                 switch (n.getNodeType()) {
                     case Node.TEXT_NODE:
+                        textNodes.add(n);
                         String text = Text.as(n).getData();
                         handler.characters(text.toCharArray(), 0, text.length());
                         return false;
@@ -66,9 +72,13 @@ class DomToSaxParser {
 
     /**
      * This will generate sax events for the DOM tree rooted at e to the provided ContentHandler.
+     *
+     * @Return A list of the text nodes (in order).
      */
-    static void parse(Element e, ContentHandler handler) {
-        new DomWalker(new DomToSaxVisitor(handler)).walk(e);
+    static List<Node> parse(Element e, ContentHandler handler) {
+        DomToSaxVisitor visitor = new DomToSaxVisitor(handler);
+        new DomWalker(visitor).walk(e);
+        return visitor.textNodes;
     }
 
     /**
