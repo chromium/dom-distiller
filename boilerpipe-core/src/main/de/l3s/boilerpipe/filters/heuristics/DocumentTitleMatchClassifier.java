8416adabd32f9c36a518d5d9e224d9fc6d81548f
Yaron Friedman <yfriedman@chromium.org>
2014-07-02 14:40:52 -0700

    Allow DocumentTitleMatchClassifier to match repeated titles.
    It's not uncommon for the title to appear multiple times in a given
    page. This is commonly because of breadcrumbs or table of contents.
    Allow all of these to be labeled as title and stripped.
    
    Some sample pages:
    http://www.cnet.com/news/facebooks-sandberg-we-really-regret-our-secret-test-misfire/
    http://chealth.canoe.ca/channel_section_details.asp?text_id=5973&channel_id=10&relation_id=3931
    http://www.washingtonpost.com/news/morning-mix/wp/2014/05/05/how-harvard-scholars-may-have-been-duped-by-a-forged-gospel-of-jesuss-wife/
    
    R=cjhopman@chromium.org
    
    Review URL: https://codereview.chromium.org/364023003

--- boilerpipe-core/src/main/de/l3s/boilerpipe/filters/heuristics/DocumentTitleMatchClassifier.java
+++ boilerpipe-core/src/main/de/l3s/boilerpipe/filters/heuristics/DocumentTitleMatchClassifier.java
@@ -1,3 +1,7 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /**
  * boilerpipe
  *
@@ -17,157 +21,154 @@
  */
 package de.l3s.boilerpipe.filters.heuristics;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.regex.Pattern;
-
 import de.l3s.boilerpipe.BoilerpipeFilter;
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.document.TextBlock;
 import de.l3s.boilerpipe.document.TextDocument;
 import de.l3s.boilerpipe.labels.DefaultLabels;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 /**
  * Marks {@link TextBlock}s which contain parts of the HTML
  * <code>&lt;TITLE&gt;</code> tag, using some heuristics which are quite
  * specific to the news domain.
- * 
+ *
  * @author Christian Kohlschütter
  */
 public final class DocumentTitleMatchClassifier implements BoilerpipeFilter {
 
-	private final Set<String> potentialTitles;
+    private final Set<String> potentialTitles;
 
-	public DocumentTitleMatchClassifier(String title) {
-		if (title == null) {
-			this.potentialTitles = null;
-		} else {
-			
-			title = title.replace('\u00a0', ' ');
-			title = title.replace("'", "");
-			
-			title = title.trim().toLowerCase();
-			
-			if (title.length() == 0) {
-				this.potentialTitles = null;
-			} else {
-				this.potentialTitles = new HashSet<String>();
+    public DocumentTitleMatchClassifier(String title) {
+        if (title == null) {
+            this.potentialTitles = null;
+        } else {
 
-				potentialTitles.add(title);
+            title = title.replace('\u00a0', ' ');
+            title = title.replace("'", "");
 
-				String p;
+            title = title.trim().toLowerCase();
 
-				p = getLongestPart(title, "[ ]*[\\|»|-][ ]*");
-				if (p != null) {
-					potentialTitles.add(p);
-				}
-				p = getLongestPart(title, "[ ]*[\\|»|:][ ]*");
-				if (p != null) {
-					potentialTitles.add(p);
-				}
-				p = getLongestPart(title, "[ ]*[\\|»|:\\(\\)][ ]*");
-				if (p != null) {
-					potentialTitles.add(p);
-				}
-				p = getLongestPart(title, "[ ]*[\\|»|:\\(\\)\\-][ ]*");
-				if (p != null) {
-					potentialTitles.add(p);
-				}
-				p = getLongestPart(title, "[ ]*[\\|»|,|:\\(\\)\\-][ ]*");
-				if (p != null) {
-					potentialTitles.add(p);
-				}
-				p = getLongestPart(title, "[ ]*[\\|»|,|:\\(\\)\\-\u00a0][ ]*");
-				if (p != null) {
-					potentialTitles.add(p);
-				}
-				
-				addPotentialTitles(potentialTitles, title, "[ ]+[\\|][ ]+", 4);
-				addPotentialTitles(potentialTitles, title, "[ ]+[\\-][ ]+", 4);
-				
-				potentialTitles.add(title.replaceFirst(" - [^\\-]+$", ""));
-				potentialTitles.add(title.replaceFirst("^[^\\-]+ - ", ""));
-			}
-		}
-	}
+            if (title.length() == 0) {
+                this.potentialTitles = null;
+            } else {
+                this.potentialTitles = new HashSet<String>();
 
-	public Set<String> getPotentialTitles() {
-		return potentialTitles;
-	}
-	
-	private void addPotentialTitles(final Set<String> potentialTitles, final String title, final String pattern, final int minWords) {
-		String[] parts = title.split(pattern);
-		if (parts.length == 1) {
-			return;
-		}
-		for (int i = 0; i < parts.length; i++) {
-			String p = parts[i];
-			if (p.contains(".com")) {
-				continue;
-			}
-			final int numWords = p.split("[\b ]+").length;
-			if (numWords >=minWords) {
-				potentialTitles.add(p);
-			}
-		}
-	}
+                potentialTitles.add(title);
 
-	private String getLongestPart(final String title, final String pattern) {
-		String[] parts = title.split(pattern);
-		if (parts.length == 1) {
-			return null;
-		}
-		int longestNumWords = 0;
-		String longestPart = "";
-		for (int i = 0; i < parts.length; i++) {
-			String p = parts[i];
-			if (p.contains(".com")) {
-				continue;
-			}
-			final int numWords = p.split("[\b ]+").length;
-			if (numWords > longestNumWords || p.length() > longestPart.length()) {
-				longestNumWords = numWords;
-				longestPart = p;
-			}
-		}
-		if (longestPart.length() == 0) {
-			return null;
-		} else {
-			return longestPart.trim();
-		}
-	}
-	
-	private static final Pattern PAT_REMOVE_CHARACTERS = Pattern.compile("[\\?\\!\\.\\-\\:]+");
+                String p;
 
-	public boolean process(TextDocument doc)
-			throws BoilerpipeProcessingException {
-		if (potentialTitles == null) {
-			return false;
-		}
-		boolean changes = false;
-		
-		for (final TextBlock tb : doc.getTextBlocks()) {
-			String text = tb.getText();
-			
-			text = text.replace('\u00a0', ' ');
-			text = text.replace("'", "");
+                p = getLongestPart(title, "[ ]*[\\|»|-][ ]*");
+                if (p != null) {
+                    potentialTitles.add(p);
+                }
+                p = getLongestPart(title, "[ ]*[\\|»|:][ ]*");
+                if (p != null) {
+                    potentialTitles.add(p);
+                }
+                p = getLongestPart(title, "[ ]*[\\|»|:\\(\\)][ ]*");
+                if (p != null) {
+                    potentialTitles.add(p);
+                }
+                p = getLongestPart(title, "[ ]*[\\|»|:\\(\\)\\-][ ]*");
+                if (p != null) {
+                    potentialTitles.add(p);
+                }
+                p = getLongestPart(title, "[ ]*[\\|»|,|:\\(\\)\\-][ ]*");
+                if (p != null) {
+                    potentialTitles.add(p);
+                }
+                p = getLongestPart(title, "[ ]*[\\|»|,|:\\(\\)\\-\u00a0][ ]*");
+                if (p != null) {
+                    potentialTitles.add(p);
+                }
 
-			text = text.trim().toLowerCase();
+                addPotentialTitles(potentialTitles, title, "[ ]+[\\|][ ]+", 4);
+                addPotentialTitles(potentialTitles, title, "[ ]+[\\-][ ]+", 4);
 
-			if (potentialTitles.contains(text)) {
-				tb.addLabel(DefaultLabels.TITLE);
-				changes = true;
-				break;
-			}
-			
-			text = PAT_REMOVE_CHARACTERS.matcher(text).replaceAll("").trim();
-			if (potentialTitles.contains(text)) {
-				tb.addLabel(DefaultLabels.TITLE);
-				changes = true;
-				break;
-			}
-		}
-		return changes;
-	}
+                potentialTitles.add(title.replaceFirst(" - [^\\-]+$", ""));
+                potentialTitles.add(title.replaceFirst("^[^\\-]+ - ", ""));
+            }
+        }
+    }
 
+    public Set<String> getPotentialTitles() {
+        return potentialTitles;
+    }
+
+    private void addPotentialTitles(final Set<String> potentialTitles, final String title, final String pattern, final int minWords) {
+        String[] parts = title.split(pattern);
+        if (parts.length == 1) {
+            return;
+        }
+        for (int i = 0; i < parts.length; i++) {
+            String p = parts[i];
+            if (p.contains(".com")) {
+                continue;
+            }
+            final int numWords = p.split("[\b ]+").length;
+            if (numWords >=minWords) {
+                potentialTitles.add(p);
+            }
+        }
+    }
+
+    private String getLongestPart(final String title, final String pattern) {
+        String[] parts = title.split(pattern);
+        if (parts.length == 1) {
+            return null;
+        }
+        int longestNumWords = 0;
+        String longestPart = "";
+        for (int i = 0; i < parts.length; i++) {
+            String p = parts[i];
+            if (p.contains(".com")) {
+                continue;
+            }
+            final int numWords = p.split("[\b ]+").length;
+            if (numWords > longestNumWords || p.length() > longestPart.length()) {
+                longestNumWords = numWords;
+                longestPart = p;
+            }
+        }
+        if (longestPart.length() == 0) {
+            return null;
+        } else {
+            return longestPart.trim();
+        }
+    }
+
+    private static final Pattern PAT_REMOVE_CHARACTERS = Pattern.compile("[\\?\\!\\.\\-\\:]+");
+
+    @Override
+    public boolean process(TextDocument doc) throws BoilerpipeProcessingException {
+        if (potentialTitles == null) {
+            return false;
+        }
+        boolean changes = false;
+
+        for (final TextBlock tb : doc.getTextBlocks()) {
+            String text = tb.getText();
+
+            text = text.replace('\u00a0', ' ');
+            text = text.replace("'", "");
+
+            text = text.trim().toLowerCase();
+
+            if (potentialTitles.contains(text)) {
+                tb.addLabel(DefaultLabels.TITLE);
+                changes = true;
+            }
+
+            text = PAT_REMOVE_CHARACTERS.matcher(text).replaceAll("").trim();
+            if (potentialTitles.contains(text)) {
+                tb.addLabel(DefaultLabels.TITLE);
+                changes = true;
+            }
+        }
+        return changes;
+    }
 }
---
+++ test/com/dom_distiller/client/DocumentTitleMatchClassifierTest.java
@@ -0,0 +1,86 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package com.dom_distiller.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import de.l3s.boilerpipe.document.TextBlock;
+import de.l3s.boilerpipe.document.TextDocument;
+import de.l3s.boilerpipe.filters.heuristics.DocumentTitleMatchClassifier;
+import de.l3s.boilerpipe.labels.DefaultLabels;
+
+import java.util.Arrays;
+
+public class DocumentTitleMatchClassifierTest extends GWTTestCase {
+    private static final String CONTENT_TEXT = "Lorem Ipsum Lorem Ipsum Lorem Ipsum.";
+    private static final String TITLE_TEXT = "I am the document title";
+
+    @Override
+    public String getModuleName() {
+        return "com.dom_distiller.DomDistillerJUnit";
+    }
+
+    public void testLabelsTitle() throws BoilerpipeProcessingException {
+        DocumentTitleMatchClassifier classifier = new DocumentTitleMatchClassifier(TITLE_TEXT);
+
+        TextBlock titleBlock = new TextBlock(TITLE_TEXT);
+        TextBlock contentBlock = new TextBlock(CONTENT_TEXT);
+        TextDocument document = new TextDocument(
+                Arrays.asList(new TextBlock[]{titleBlock, contentBlock}));
+        classifier.process(document);
+
+        assertTrue(titleBlock.hasLabel(DefaultLabels.TITLE));
+        assertFalse(contentBlock.hasLabel(DefaultLabels.TITLE));
+    }
+
+    // Mimics leading and trailing breadcrumbs containing the title.
+    public void testLabelsMultipeTitle() throws BoilerpipeProcessingException {
+        DocumentTitleMatchClassifier classifier = new DocumentTitleMatchClassifier(TITLE_TEXT);
+
+        TextBlock titleBlockAsLi = new TextBlock(TITLE_TEXT);
+        titleBlockAsLi.addLabel(DefaultLabels.LI);
+        TextBlock titleBlock = new TextBlock(TITLE_TEXT);
+        TextBlock contentBlock = new TextBlock(CONTENT_TEXT);
+        TextBlock trailingTitleBlockAsLi = new TextBlock(TITLE_TEXT);
+        trailingTitleBlockAsLi.addLabel(DefaultLabels.LI);
+
+        TextDocument document = new TextDocument(
+                Arrays.asList(new TextBlock[]{titleBlockAsLi, titleBlock, contentBlock, trailingTitleBlockAsLi}));
+        classifier.process(document);
+
+
+        assertTrue(titleBlockAsLi.hasLabel(DefaultLabels.TITLE));
+        assertTrue(titleBlock.hasLabel(DefaultLabels.TITLE));
+        assertFalse(contentBlock.hasLabel(DefaultLabels.TITLE));
+        assertTrue(trailingTitleBlockAsLi.hasLabel(DefaultLabels.TITLE));
+    }
+
+    public void testDoesNotLabelTitleInContent() throws BoilerpipeProcessingException {
+        DocumentTitleMatchClassifier classifier = new DocumentTitleMatchClassifier(TITLE_TEXT);
+
+        TextBlock titleAndContentBlock = new TextBlock(TITLE_TEXT + " " + CONTENT_TEXT);
+
+        TextDocument document = new TextDocument(Arrays.asList(new TextBlock[]{titleAndContentBlock}));
+        classifier.process(document);
+
+        assertFalse(titleAndContentBlock.hasLabel(DefaultLabels.TITLE));
+    }
+
+    // Non-exhaustive test for the type of partial-matches that Boilerpipe performs.
+    public void testLabelsPartialTitleMatch() throws BoilerpipeProcessingException {
+        DocumentTitleMatchClassifier classifier = new DocumentTitleMatchClassifier(
+                "BreakingNews » " + TITLE_TEXT);
+
+        TextBlock titleBlock = new TextBlock(TITLE_TEXT);
+        TextBlock contentBlock = new TextBlock(CONTENT_TEXT);
+        TextDocument document = new TextDocument(
+                Arrays.asList(new TextBlock[]{titleBlock, contentBlock}));
+        classifier.process(document);
+
+        assertTrue(titleBlock.hasLabel(DefaultLabels.TITLE));
+        assertFalse(contentBlock.hasLabel(DefaultLabels.TITLE));
+    }
+}
