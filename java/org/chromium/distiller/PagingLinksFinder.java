chromium/dom-distiller

commit 7b12bc778d1be96c4fb68a429f567f65bbe53fae

Resolve links in a new HTML document with a base tag
This simplifies link resolution, but the speed is still roughly
the same as manual resolution. Perf scoring goes through the same
code path as the real case, so it's more realistic now.

R=cjhopman@chromium.org

Review URL: https://codereview.chromium.org/901843002

java/org/chromium/distiller/PagingLinksFinder.java
@@ -14,6 +14,7 @@ package org.chromium.distiller;
 import org.chromium.distiller.proto.DomDistillerProtos;
 
 import com.google.gwt.dom.client.AnchorElement;
+import com.google.gwt.dom.client.BaseElement;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NodeList;
@@ -104,6 +105,8 @@ public class PagingLinksFinder {
         NodeList<Element> allLinks = root.getElementsByTagName("A");
         Map<String, PagingLinkObj> possiblePages = new HashMap<String, PagingLinkObj>();
 
+        AnchorElement baseAnchor = createAnchorWithBase(original_url);
+
         // Loop through all links, looking for hints that they may be next- or previous- page links.
         // Things like having "page" in their textContent, className or id, or being a child of a
         // node with a page-y className or id.
@@ -127,7 +130,7 @@ public class PagingLinksFinder {
             // Remove url anchor and then trailing '/' from link's href.
             // Note that AnchorElement.getHref() returns the absolute URI, so there's no need to
             // worry about relative links.
-            String linkHref = REG_HREF_CLEANER.replace(resolveLinkHref(link, original_url), "");
+            String linkHref = REG_HREF_CLEANER.replace(resolveLinkHref(link, baseAnchor), "");
             appendDbgStrForLink(link, "-> " + linkHref);
 
             // Ignore page link that is empty, not http/https, or same as current window location.
@@ -326,50 +329,29 @@ public class PagingLinksFinder {
         return pagingHref;
     }
 
+    public static AnchorElement createAnchorWithBase(String base_url) {
+        Document doc = DomUtil.createHTMLDocument(Document.get());
+
+        BaseElement base = doc.createBaseElement();
+        base.setHref(base_url);
+        doc.getHead().appendChild(base);
+
+        AnchorElement a = doc.createAnchorElement();
+        doc.getBody().appendChild(base);
+        return a;
+    }
+
     private static String fixMissingScheme(String url) {
         if (url.isEmpty()) return "";
         if (!url.contains("://")) return "http://" + url;
         return url;
     }
 
-    // The absolute URI of the link is used in production mode.
-    // In testing or scoring, where original_url is given, original_url is treated as if it is
-    // Window.Location.getHref().
-    public static String resolveLinkHref(AnchorElement link, String original_url) {
-        if (original_url.equals(Window.Location.getHref())) {
-            return link.getHref();
-        }
+    // The link is resolved using an anchor within a new HTML document with a base tag.
+    public static String resolveLinkHref(AnchorElement link, AnchorElement baseAnchor) {
         String linkHref = REG_HREF_CLEANER.replace(link.getAttribute("href"), "");
-
-        if (linkHref.substring(0, 1) == "/" ) {
-            return getScheme(original_url) + "://" + getHostname(original_url) + linkHref;
-        } else if (linkHref.contains("://") || linkHref.startsWith("javascript:")) {
-            return linkHref;
-        } else {
-            String ans = getScheme(original_url) + "://" + getHostname(original_url);
-            List<String> pathStack = new ArrayList<String>();
-            String[] pathArray = StringUtil.split(getPath(original_url), "\\/");
-            for (String e: pathArray) {
-                if(!e.isEmpty()) pathStack.add(e);
-            }
-            if (pathStack.size() > 0) {
-                pathStack.remove(pathStack.size()-1);
-            }
-            String[] dirs = StringUtil.split(linkHref, "\\/");
-            for (int i = 0; i < dirs.length; i++) {
-                if (dirs[i] == "..") {
-                    if (pathStack.size() > 0) {
-                        pathStack.remove(pathStack.size()-1);
-                    }
-                } else {
-                    pathStack.add(dirs[i]);
-                }
-            }
-            for (int i = 0; i < pathStack.size(); i++) {
-                ans += "/" + pathStack.get(i);
-            }
-            return ans;
-        }
+        baseAnchor.setAttribute("href", linkHref);
+        return baseAnchor.getHref();
     }
 
     private static String getScheme(String url) {

java/org/chromium/distiller/DomUtil.java
@@ -166,4 +166,8 @@ public class DomUtil {
     public static native NodeList<Element> querySelectorAll(Node l, String selectors) /*-{
         return l.querySelectorAll(selectors);
     }-*/;
+
+    public static native Document createHTMLDocument(Document doc) /*-{
+        return doc.implementation.createHTMLDocument();
+    }-*/;
 }

javatests/org/chromium/distiller/PagingLinksFinderTest.java
@@ -15,7 +15,8 @@ public class PagingLinksFinderTest extends DomDistillerJsTestCase {
 
     private static void checkResolveLinkHref(AnchorElement anchor, String original_url, String expected, String href) {
         anchor.setHref(href);
-        assertEquals(expected, PagingLinksFinder.resolveLinkHref(anchor, original_url));
+        AnchorElement baseAnchor = PagingLinksFinder.createAnchorWithBase(original_url);
+        assertEquals(expected, PagingLinksFinder.resolveLinkHref(anchor, baseAnchor));
     }
 
     public void testResolveLinkHref() {
@@ -40,16 +41,17 @@ public class PagingLinksFinderTest extends DomDistillerJsTestCase {
     }
 
     private static void checkLinks(AnchorElement next, AnchorElement prev, Element root, String original_url) {
+        AnchorElement baseAnchor = PagingLinksFinder.createAnchorWithBase(original_url);
         if (next == null) {
             assertNull(PagingLinksFinder.findNext(root, original_url));
         } else {
-            String href = PagingLinksFinder.resolveLinkHref(next, original_url);
+            String href = PagingLinksFinder.resolveLinkHref(next, baseAnchor);
             assertEquals(href, PagingLinksFinder.findNext(root, original_url));
         }
         if (prev == null) {
             assertNull(PagingLinksFinder.findPrevious(root, original_url));
         } else {
-            String href = PagingLinksFinder.resolveLinkHref(prev, original_url);
+            String href = PagingLinksFinder.resolveLinkHref(prev, baseAnchor);
             assertEquals(href, PagingLinksFinder.findPrevious(root, original_url));
         }
     }

commit 3e49cf2112ff22b825aabcc170e504726e547f00

Fix flakiness due to a bug in createAnchorWithBase()
The anchor element was not inserted into the new document with the
base tag. However, the result was correct most of the time, which
is quite surprising.

This bug was introduced in revision 7b12bc7 "Resolve links in a new
HTML document with a base tag".

R=cjhopman@chromium.org

Review URL: https://codereview.chromium.org/955193002

java/org/chromium/distiller/PagingLinksFinder.java
@@ -365,7 +365,7 @@ public class PagingLinksFinder {
         doc.getHead().appendChild(base);
 
         AnchorElement a = doc.createAnchorElement();
-        doc.getBody().appendChild(base);
+        doc.getBody().appendChild(a);
         return a;
     }
 