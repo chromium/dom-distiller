d4854eb5387e769863c5a26d5e2ac2af5f86f8e8
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2015-04-30 12:19:26 -0700

    Catch errors in continuous integration script
    This fixes a bug introduced in the following revision:
    "Put steps in continuous integration into a bash script".
    https://codereview.chromium.org/952783006
    
    Also print the commands being run.
    
    Allow "start xvfb" to fail in order to run locally.
    
    BUG=483149
    R=nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/1115583005

--- run-continuous-integration.sh
+++ run-continuous-integration.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -e
+set -x
+
 git log --oneline -n 5
 sudo ./install-build-deps.sh
-sudo start xvfb
+sudo start xvfb || true
 sudo ant test -Dtest.filter='*-*.ParsedUrlTest.*:*PagePatternTest.testIsPagePatternValid:*PagePatternTest.testIsPagingUrl'
 sudo ant package
