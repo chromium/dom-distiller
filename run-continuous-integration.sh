640e3993b518e71a985fdedbfb1f034bd22359a7
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2015-06-02 02:27:56 -0700

    Disable sandbox and run all the tests in continuous integration
    Now that we can update Chrome version to where all tests are supported
    on our continuous integration service, drone.io, there's no reason to
    skip tests.
    
    This depends on "Update Chrome if it is too old"
    (https://crrev.com/1159743006/) and "Allow tests to run without Chrome
    sandbox" (https://crrev.com/1164683006/).
    
    BUG=483149, 495254
    R=cjhopman@chromium.org, nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/1161023005

--- run-continuous-integration.sh
+++ run-continuous-integration.sh
@@ -6,5 +6,5 @@ set -x
 git log --oneline -n 5
 sudo ./install-build-deps.sh
 sudo start xvfb || true
-sudo ant test -Dtest.shuffle=1 -Dtest.repeat=10 -Dtest.no_sandbox=1 -Dtest.filter='-*.ParsedUrlTest.*:*PagePatternTest.testIsPagePatternValid:*PagePatternTest.testIsPagingUrl'
+sudo ant test -Dtest.shuffle=1 -Dtest.repeat=10 -Dtest.no_sandbox=1
 sudo ant package
