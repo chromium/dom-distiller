ba2c5d91877382862ce858f25935786daa9a9cd3
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-07-25 01:01:16 -0700

    Adapt new version scheme of Chrome Driver
    The version scheme changed after 2.46.
    See http://chromedriver.chromium.org/downloads/version-selection
    
    Bug: None
    Change-Id: Ibda074ee43c241a26f6352b3335e46c1c9804fbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/dom-distiller/+/1717928
    Reviewed-by: Matthew Jones <mdjones@chromium.org>

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -65,7 +65,12 @@
   mkdir $tmp
   cd $tmp
 
-  wget https://chromedriver.storage.googleapis.com/2.24/$zip
+  # The version scheme changed after 2.46.
+  # See http://chromedriver.chromium.org/downloads/version-selection
+  CHROME_VERSION=$(google-chrome --version | tr " " "\n" | awk '/[0-9.]/{print ($1+0)}')
+  VERSION=$(wget -q --output-document - https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION} | cat)
+  [ -z "${VERSION}" ] && VERSION=2.24
+  wget https://chromedriver.storage.googleapis.com/${VERSION}/$zip
   chmod a+r $zip
   sudo -u $user mkdir -p $tools
   sudo -u $user unzip -o -d $tools $zip
