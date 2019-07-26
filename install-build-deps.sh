a5e67af83bd7a7d672650e83bda6b394d5254ae8
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-07-26 16:12:19 -0700

    Update Travis CI environment from Ubuntu 14.04 to 18.04
    Manually downgrades Java to JDK 8 since Open JDK 8 is no longer
    supported on Ubuntu 18.04.
    
    Bug: None
    Change-Id: Id6a0d1a0bcfceed2c0e6e3530d49f4fc93f73b4d

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -28,6 +28,9 @@
     xdotool \
     xvfb
 
+  # Specify JDK version in case there are other versions installed.
+  update-alternatives --set java $(sudo update-alternatives --list java | grep java-8)
+
   if ! command -v google-chrome >/dev/null 2>&1; then
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
     echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
@@ -65,8 +68,7 @@
   mkdir $tmp
   cd $tmp
 
-  # The version scheme changed after 2.46.
-  # See http://chromedriver.chromium.org/downloads/version-selection
+  # The version scheme changed after 2.46. See http://chromedriver.chromium.org/downloads/version-selection
   CHROME_VERSION=$(google-chrome --version | tr " " "\n" | awk '/[0-9.]/{print ($1+0)}')
   VERSION=$(wget -q --output-document - https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION} | cat)
   [ -z "${VERSION}" ] && VERSION=2.24
